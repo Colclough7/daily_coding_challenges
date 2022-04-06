681824b40dbde8c7bba5bc13ab2edb5b24cda4e8
Colclough7 <ericcolclough@gmail.com>
2022-04-05 20:19:55 -0400

    Update giveMeADiamond.js

--- giveMeADiamond.js
+++ giveMeADiamond.js
@@ -25,4 +25,15 @@ A size 5 diamond:
 
 "  *\n ***\n*****\n ***\n  *\n"
 */
-
+function diamond(n){
+  if (n < 0 || !(n % 2)) return null; 
+  const middleIndex = Math.floor(n / 2);
+  
+  return Array.apply(null, {length: n})
+      .map((el, index) => {
+        const indentation = Math.abs(index - middleIndex);
+        const numberOfAsterisks = n - indentation * 2;
+        return Array(indentation + 1).join(' ') + Array(numberOfAsterisks + 1).join('*');
+      })
+      .join('\n') + '\n';
+}
