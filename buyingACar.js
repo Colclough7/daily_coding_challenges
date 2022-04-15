33f1dd8c57c699c474c735eb688a53b2397f88b6
Colclough7 <ericcolclough@gmail.com>
2022-04-15 13:19:24 -0400

    Update buyingACar.js

--- buyingACar.js
+++ buyingACar.js
@@ -24,14 +24,25 @@ end month 4: percentLoss 2.5 available -1534.06489...
 end month 5: percentLoss 2.5 available -395.71327...
 end month 6: percentLoss 3.0 available 766.158120825...
 return [6, 766] or (6, 766)
-where 6 is the number of months at the end of which he can buy the new car and 766 is the nearest integer to 766.158... (rounding 766.158 gives 766).
+where 6 is the number of months at the end of which he can 
+buy the new car and 766 is the nearest integer to 766.158... (rounding 766.158 gives 766).
 
 Note:
 
-Selling, buying and saving are normally done at end of month. Calculations are processed at the end of each considered month but if, by chance from the start, the value of the old car is bigger than the value of the new one or equal there is no saving to be made, no need to wait so he can at the beginning of the month buy the new car:
+Selling, buying and saving are normally done at end of month. 
+Calculations are processed at the end of each considered month but if, 
+by chance from the start, the value of the old car is bigger than the value 
+of the new one or equal there is no saving to be made, no need to wait so he 
+can at the beginning of the month buy the new car:
 
 nbMonths(12000, 8000, 1000, 1.5) should return [0, 4000]
 nbMonths(8000, 8000, 1000, 1.5) should return [0, 0]
 We don't take care of a deposit of savings in a bank:-)
 
 */
+
+
+
+
+
+
