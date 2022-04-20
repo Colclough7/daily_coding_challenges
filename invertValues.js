593b86bac5491e44b5cea76be6fcb0e45077b00f
Colclough7 <ericcolclough@gmail.com>
2022-04-19 22:11:51 -0400

    Update invertValues.js

--- invertValues.js
+++ invertValues.js
@@ -5,4 +5,4 @@ invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 You can assume that all values are integers. Do not mutate the input array/list.*/
 
-const invert = array => array.map(num => -num
+const invert = array => array.map(num => -num)
