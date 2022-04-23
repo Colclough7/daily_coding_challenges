639bba3f21cdcf5563eee647ee87cfda67a52a00
Colclough7 <ericcolclough@gmail.com>
2022-04-23 17:32:20 -0400

    Update calculateAverage.js

--- calculateAverage.js
+++ calculateAverage.js
@@ -2,3 +2,9 @@
 
 Note: Empty arrays should return 0.*/
 
+function find_average(array) {
+  if(array.length === 0)return 0
+  else{
+   return array.reduce((a, b) => (a + b)) / array.length;
+  }
+}
