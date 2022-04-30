a27fbdf135979cf275d084f36fe4df5fdaeaa074
Colclough7 <ericcolclough@gmail.com>
2022-04-29 21:38:47 -0400

    Update detectPangram.js

--- detectPangram.js
+++ detectPangram.js
@@ -3,5 +3,7 @@ For example, the sentence "The quick brown fox jumps over the lazy dog" is a pan
 
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.*/
 
-
+function isPangram(string){
+  return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+}
 
