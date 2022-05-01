7441f6d0cd08054a072ee338bc2bdd79efd5014d
Colclough7 <ericcolclough@gmail.com>
2022-04-30 22:40:12 -0400

    Update stringRepeat.js

--- stringRepeat.js
+++ stringRepeat.js
@@ -1,4 +1,7 @@
-Write a function called repeatStr which repeats the given string string exactly n times.
+/*Write a function called repeatStr which repeats the given string string exactly n times.
 
 repeatStr(6, "I") // "IIIIII"
-repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"*/
+
+
+
