29cded96500a88ace39a3c477dd7116570b5d849
Colclough7 <ericcolclough@gmail.com>
2022-05-03 10:29:52 -0400

    Update sortTheOdd.js

--- sortTheOdd.js
+++ sortTheOdd.js
@@ -5,3 +5,12 @@ Examples
 [7, 1]  =>  [1, 7]
 [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0] */
+
+
+function sortArray(array) {
+ const odds = array
+    .filter( item => item % 2)
+    .sort((a,b) => a < b ? -1 : 1)
+  return array
+    .map( item => item % 2 ? odds.shift() : item)
+}
