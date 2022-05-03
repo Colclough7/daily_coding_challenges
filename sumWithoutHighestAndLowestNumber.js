3933461d83ea4ffa9384c2e14eca4c1320001e4d
Colclough7 <ericcolclough@gmail.com>
2022-05-03 10:18:25 -0400

    Update sumWithoutHighestAndLowestNumber.js

--- sumWithoutHighestAndLowestNumber.js
+++ sumWithoutHighestAndLowestNumber.js
@@ -13,3 +13,9 @@ If an empty value ( null, None, Nothing etc. ) is given instead of an array,
   or the given array is an empty list or a list with only 1 element, return 0.*/
 
 
+
+function sumArray(array) {
+return !array || array.length==0?0:array.sort((a,b)=> a - b).slice(1,array.length - 1).reduce((acc,curr)=> acc + curr,0)
+}
+
+
