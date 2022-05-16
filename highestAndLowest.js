aafab83918b3b267279d2d9882a80fa6c0ebf66d
Colclough7 <ericcolclough@gmail.com>
2022-05-15 22:22:21 -0400

    Update highestAndLowest.js

--- highestAndLowest.js
+++ highestAndLowest.js
@@ -9,3 +9,10 @@ All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.*/
 
+
+
+function highAndLow(numbers){
+ const arr = numbers.split(" ").map(Number)
+    return `${Math.max(...arr)} ${Math.min(...arr)}`
+}
+
