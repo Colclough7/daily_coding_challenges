c3f62cdc1328b236ff6a4a454e2ddbc14c4a75a6
Colclough7 <ericcolclough@gmail.com>
2022-05-17 14:40:31 -0400

    Update countCharactersInYourString.js

--- countCharactersInYourString.js
+++ countCharactersInYourString.js
@@ -4,3 +4,12 @@ If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 What if the string is empty? Then the result should be empty object literal, {}.*/
 
 
+function count (string) {  
+  return string === ''? {}:[...string].reduce((total,letter)=>{
+    total[letter] ? total[letter]++ : total[letter] = 1;
+    return total;
+  } ,{})
+}
+
+
+
