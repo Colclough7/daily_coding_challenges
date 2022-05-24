e58155f08981a5069cb7205c8849e0b2703135c3
Colclough7 <ericcolclough@gmail.com>
2022-05-24 17:15:32 -0400

    Update takeATenMinutesWalk.js

--- takeATenMinutesWalk.js
+++ takeATenMinutesWalk.js
@@ -2,3 +2,27 @@
 You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, 
 so create a function that will return true if the walk the app gives you will take you exactly ten minutes 
 (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.*/
+
+
+
+
+
+function isValidWalk(walk) {
+  if (walk.length !== 10)
+    return false;
+  var dir = {
+    n:0,
+    s:0,
+    w:0,
+    e:0,
+  }
+  for (var i = 0; i < walk.length; i++) {
+    dir[walk[i]]++;
+  }
+  
+  if (dir["n"] !== dir["s"] || dir["w"] !== dir["e"])
+    return false;
+  else {
+    return true;
+  }
+}
