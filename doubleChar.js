0f51ea2e26d5d1763d9caefd1608bb0dd683881b
Colclough7 <ericcolclough@gmail.com>
2022-05-28 15:35:14 -0400

    Update doubleChar.js

--- doubleChar.js
+++ doubleChar.js
@@ -4,3 +4,14 @@ Examples (Input -> Output):
 * "String"      -> "SSttrriinngg"
 * "Hello World" -> "HHeelllloo  WWoorrlldd"
 * "1234!_ "     -> "11223344!!__  "*/
+
+
+
+
+function doubleChar(str) {
+  var newStr = '';
+  for (var i = 0; i < str.length; i++){
+    newStr += str[i] + str[i];
+  }
+  return newStr;
+}
