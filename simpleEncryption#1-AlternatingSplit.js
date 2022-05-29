1fdaecf6e57edf9a893ab1e99beaa4d22d899619
Colclough7 <ericcolclough@gmail.com>
2022-05-29 15:05:18 -0400

    Update simpleEncryption#1-AlternatingSplit.js

--- simpleEncryption#1-AlternatingSplit.js
+++ simpleEncryption#1-AlternatingSplit.js
@@ -13,3 +13,20 @@ encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
 Together with the encryption function, you should also implement a decryption function which reverses the process.
 
 If the string S is an empty value or the integer N is not positive, return the first argument without changes.*/
+
+
+
+function encrypt(text, n) {
+  if (!text || n <= 0) return text; 
+  while (n--) {
+    let ans = '';
+    for (let i = 1; i < text.length; i += 2) {
+      ans += text[i];
+    }
+    for (let i = 0; i < text.length; i += 2) {
+      ans += text[i];
+    }
+    text = ans;
+  }
+  return text;
+}
