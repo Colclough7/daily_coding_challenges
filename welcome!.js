91cad52026e11c58f5df3c3d23b1de6aa581a6a9
Colclough7 <ericcolclough@gmail.com>
2022-06-13 04:17:00 -0400

    Update welcome!.js

--- welcome!.js
+++ welcome!.js
@@ -29,3 +29,26 @@ Possible invalid inputs include:
 IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
 IP_ADDRESS_NOT_FOUND - ip address not in the database
 IP_ADDRESS_REQUIRED - no ip address was supplied*/
+
+function greet(language) {
+	const database = {
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+  }
+   return database[language] || database.english
+}
