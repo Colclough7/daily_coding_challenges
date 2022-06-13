bb4e352effe08ad9f708c7052da7589425950c4c
Colclough7 <ericcolclough@gmail.com>
2022-06-12 23:50:31 -0400

    Update returnNegative.js

--- returnNegative.js
+++ returnNegative.js
@@ -9,3 +9,11 @@ Notes
 The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.*/
 
+
+
+
+
+function makeNegative(num) {
+  return Math.sign(num) === 1?-Math.abs(num):num
+}
+
