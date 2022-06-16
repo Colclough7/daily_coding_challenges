b598e06138e2c0e7a70dbef634edd5cd9b8bfca2
Colclough7 <ericcolclough@gmail.com>
2022-06-16 18:48:51 -0400

    Update extractTheDomainNameFromAURL.js

--- extractTheDomainNameFromAURL.js
+++ extractTheDomainNameFromAURL.js
@@ -3,3 +3,13 @@
 * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 * url = "https://www.cnet.com"                -> domain name = cnet"*/
+
+
+
+
+function domainName(url){
+  let sourceString = url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
+  let domain = sourceString.split(".")[0];
+  return domain;
+}
+
