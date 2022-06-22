551ba2608abe3e02a52d2661aa0aec4e003f59dc
Colclough7 <you@example.com>
2022-06-22 12:13:34 -0400

    Updated switchItUp.js

---  switchItUp.js
+++  switchItUp.js
@@ -6,3 +6,12 @@ Output :: "One".
 
 If your language supports it, try using a switch statement.*/
 
+
+
+
+function switchItUp(number){
+    let word = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+    return word[number]
+    
+    }
+
