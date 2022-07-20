544405340bdffb4614db972bc0deb03d9657c43d
Colclough7 <ericcolclough@gmail.com>
2022-07-20 02:54:18 -0400

    Update sumOfAllTheMultiplesOf3Or5.js

--- sumOfAllTheMultiplesOf3Or5.js
+++ sumOfAllTheMultiplesOf3Or5.js
@@ -8,3 +8,11 @@ findSum(5) should return 8 (3 + 5)
 
 findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)*/
 
+
+
+function findSum(n) {
+  let countArr = []
+  
+  for(let i = 0; i <= n; i++) if(i % 3 === 0 || i % 5 === 0) countArr.push(i) 
+  return countArr.reduce((acc , curr) => acc + curr)
+}
