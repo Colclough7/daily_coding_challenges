e406ffcbebd213f9ec71bb0f42e8e3cf34b03513
Colclough7 <ericcolclough@gmail.com>
2022-08-23 18:58:05 -0400

    Update grasshopper-CheckForFactor.js

--- grasshopper-CheckForFactor.js
+++ grasshopper-CheckForFactor.js
@@ -12,3 +12,10 @@ You can use the mod operator (%) in most languages to check for a remainder
 For example 2 is not a factor of 7 because: 7 % 2 = 1
 
 Note: base is a non-negative number, factor is a positive number.*/
+
+
+
+
+function checkForFactor (base, factor) {
+  return base%factor === 0
+}
