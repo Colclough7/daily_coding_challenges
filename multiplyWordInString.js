84aac3313389e0619cf88b10dda9247d393921d7
Colclough7 <ericcolclough@gmail.com>
2022-08-24 18:40:14 -0400

    Update multiplyWordInString.js

--- multiplyWordInString.js
+++ multiplyWordInString.js
@@ -16,3 +16,7 @@ Should return
 Since the 3rd word is 'string'(starting from 0 remember) and the third paramater indicates that it should be repeated 5 times.
 
 Simple. Good luck.*/
+
+function modifyMultiply (str,loc,num) {
+  return (str.split(' ')[loc]+'-').repeat(num).slice(0,-1)
+} 
