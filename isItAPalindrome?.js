15cd93062422a5aeff6d0830333b166eff1a150d
Colclough7 <ericcolclough@gmail.com>
2022-08-30 19:16:34 -0400

    Update isItAPalindrome?.js

--- isItAPalindrome?.js
+++ isItAPalindrome?.js
@@ -1,1 +1,7 @@
 /*Write a function that checks if a given string (case insensitive) is a palindrome.*/
+
+
+function isPalindrome(str) {
+    str = str.replace(/[^a-zA-Z0-9]+/gi, '').toLowerCase();
+    return str == str.split('').reverse().join('');
+}
