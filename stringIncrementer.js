3cc224de280e3faacb73a74dfa732139ea63e577
Colclough7 <ericcolclough@gmail.com>
2022-10-29 22:34:45 -0400

    Update stringIncrementer.js

--- stringIncrementer.js
+++ stringIncrementer.js
@@ -13,3 +13,14 @@ foo0042 -> foo0043
 foo9 -> foo10
 
 foo099 -> foo100*/
+
+
+function incrementString(str) {
+  const body = str.slice(0, -1);
+  const lastDigit = str.slice(-1).match(/[0-9]/);
+  return lastDigit === null
+    ? str + "1"
+    : lastDigit != 9
+    ? body + (+lastDigit + 1)
+    : incrementString(body) + "0";
+}
