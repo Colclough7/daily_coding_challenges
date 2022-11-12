4411b41d22a18e64c6f38b5888ae37daba3e4748
Colclough7 <ericcolclough@gmail.com>
2022-11-11 20:52:52 -0500

    Update trainingJS#4:BasicDataTypesArray.js

--- trainingJS#4:BasicDataTypesArray.js
+++ trainingJS#4:BasicDataTypesArray.js
@@ -15,3 +15,30 @@ console.log(arr)     //[1,2,3,4]
 arr.pop();           //remove the last element from arr
 console.log(arr)     //[1,2,3]
 */
+
+
+
+
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length - 1]
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  arr.push(el)
+  return arr
+}
+function popElement(arr){
+  //pop an element from arr
+  arr.pop()
+  return arr
+}
