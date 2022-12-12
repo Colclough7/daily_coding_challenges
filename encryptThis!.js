b25a1216effef00370f79e5c89c5540fa2166fe8
Colclough7 <ericcolclough@gmail.com>
2022-12-11 23:43:34 -0500

    Update encryptThis!.js

--- encryptThis!.js
+++ encryptThis!.js
@@ -16,3 +16,18 @@ encryptThis("Hello") === "72olle"
 encryptThis("good") === "103doo"
 encryptThis("hello world") === "104olle 119drlo"*/
 
+
+
+let encryptThis = function(text) {
+  // Implement me! :)
+  return text
+    .split(' ')
+    .map(e => {
+      if (e.length === 1) return e.charCodeAt(0);
+      if (e.length === 2) return `${e[0].charCodeAt(0)}${e[1]}`;
+      if (e.length === 3) return `${e[0].charCodeAt(0)}${e.slice(-1)}${e[1]}`;
+      if (e.length > 3) return `${e[0].charCodeAt(0)}${e.slice(-1)}${e.slice(2, -1)}${e[1]}`;
+    })
+    .join(' ');
+}
+
