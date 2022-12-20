b9d10da42c501aabc7a7d90e2116e391190395b0
Colclough7 <ericcolclough@gmail.com>
2022-12-20 15:26:01 -0500

    Update makeAFunctionThatDoesArithmetic!.js

--- makeAFunctionThatDoesArithmetic!.js
+++ makeAFunctionThatDoesArithmetic!.js
@@ -10,3 +10,15 @@ A few examples:(Input1, Input2, Input3 --> Output)
 5, 2, "subtract" --> 3
 5, 2, "multiply" --> 10
 5, 2, "divide"   --> 2.5*/
+
+
+
+
+function arithmetic(a, b, operator){
+  if(operator === 'add') return a + b
+  else if (operator === 'subtract') return a - b
+  else if(operator === 'multiply') return a * b
+  else if(operator === 'divide') return a / b
+  
+  
+}
