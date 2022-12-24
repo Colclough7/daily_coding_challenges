b72c942b608bf47df71f0e806b26be65ad34d270
Colclough7 <you@example.com>
2022-12-23 20:21:54 -0500

    nex to last commit

--- exesAndOhs.js
+++ exesAndOhs.js
@@ -11,4 +11,7 @@
 //XO("zzoo") => false
 
 
-
+function XO(str) {
+  let arr = str.toLowerCase().split('');
+return arr.filter(element => element == 'x').length == arr.filter(element => element == 'o').length;
+}
