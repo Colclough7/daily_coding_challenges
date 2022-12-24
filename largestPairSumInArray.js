7c3e3ab0e0d01bc3848443cc3d59bbd962c2e492
Colclough7 <ericcolclough@gmail.com>
2022-12-24 00:03:41 -0500

    Update largestPairSumInArray.js

--- largestPairSumInArray.js
+++ largestPairSumInArray.js
@@ -4,3 +4,11 @@ For example
 
 [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
 [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)*/
+
+
+
+function largestPairSum (numbers) {
+  //TODO: Write your Code here
+ let answers = numbers.sort((a,b)=>b-a)
+ return answers[0] + answers[1]
+}
