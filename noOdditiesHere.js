280caebf4dc5a7402369506f3d3ad9dfb552cf83
Colclough7 <ericcolclough@gmail.com>
2023-01-02 10:04:54 -0500

    Update noOdditiesHere.js

--- noOdditiesHere.js
+++ noOdditiesHere.js
@@ -2,3 +2,13 @@
 
 All values in the array will be integers. Return the good values in the order they are given.*/
 
+
+
+
+function noOdds( values ){
+  // Return all non-odd values
+  return values.filter(value=>{
+    return value%2===0 || 0
+  })
+}
+
