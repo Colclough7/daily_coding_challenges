70e419f2a27e61c7743013f365325569ea665d25
Colclough7 <ericcolclough@gmail.com>
2023-01-04 11:32:34 -0500

    Update removeExclamationMarks.js

--- removeExclamationMarks.js
+++ removeExclamationMarks.js
@@ -1,3 +1,7 @@
 /*Write function RemoveExclamationMarks which removes all exclamation marks from a given string.*/
 
 
+function removeExclamationMarks(s) {
+  let x = s.split('').filter(str=>str !== '!').join('')
+  return x
+}
