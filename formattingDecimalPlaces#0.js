939bbceb61dbcf187a65302fe8f4bbf4096d6cea
Colclough7 <ericcolclough@gmail.com>
2023-01-03 20:08:09 -0500

    Update formattingDecimalPlaces#0.js

--- formattingDecimalPlaces#0.js
+++ formattingDecimalPlaces#0.js
@@ -4,3 +4,11 @@ You don't need to check whether the input is a valid number because only valid n
 Example:    
 5.5589 is rounded 5.56   
 3.3424 is rounded 3.34*/
+
+
+
+
+function twoDecimalPlaces(n) {
+  // Your code here
+  return +n.toFixed(2)
+}
