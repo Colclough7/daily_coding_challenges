bf6f2cc93e3615c16bab0becdc6628d5aa18921b
Colclough7 <ericcolclough@gmail.com>
2023-01-17 01:33:37 -0500

    Update trainingJS#2:BasicDataTypes--Number.js

--- trainingJS#2:BasicDataTypes--Number.js
+++ trainingJS#2:BasicDataTypes--Number.js
@@ -8,3 +8,40 @@ every function has two local variables a,b, please set the appropriate value for
 making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.*/
 
 
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2(){
+  let a = v3 , //set number value to a
+      b = v1 ; //set number value to b
+  return a - b;
+}
+
+function equal3(){
+  let a = v1 , //set number value to a
+      b = v5 ; //set number value to b
+  return a * b;
+}
+
+function equal4(){
+  let a = v4 , //set number value to a
+      b = v5 ; //set number value to b
+  return a / b;
+}
+
+function equal5(){
+  let a = v6 , //set number value to a
+      b = v3 ; //set number value to b
+  return a % b;
+}
