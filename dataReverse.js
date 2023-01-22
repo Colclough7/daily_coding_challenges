0810a77b2fb385f986d8d94477679947b4bb56f2
Colclough7 <ericcolclough@gmail.com>
2023-01-22 16:26:14 -0500

    Update dataReverse.js

--- dataReverse.js
+++ dataReverse.js
@@ -14,3 +14,15 @@ The data is given in an array as such:
 
 [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
 Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.*/
+
+
+
+const dataReverse = function(data){
+  if (data.length===0) return []
+  const arrayBytes = [];
+  const tam = data.length / 8;
+  for (var i = 0; i < tam; i++){
+    arrayBytes.push(data.splice(0,8));
+  }
+  return arrayBytes.reverse().join().split(',').map(v=>v*1);
+}
