6b9353300200fdfb6895d04fb208facc4b406d8a
Colclough7 <ericcolclough@gmail.com>
2023-02-02 03:34:51 -0500

    Update evenNumbersInAnArray.js

--- evenNumbersInAnArray.js
+++ evenNumbersInAnArray.js
@@ -7,3 +7,14 @@ For example:
 ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]*/
 
+
+
+
+function evenNumbers(array, number) {
+  // good luck
+  const result = []
+  for(let i = 0;i<=array.length;i++){
+    if(array[i]%2===0)result.push(array[i])
+  }
+  return result.splice(result.length-number,number)
+}
