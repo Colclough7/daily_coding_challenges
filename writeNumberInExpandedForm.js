2d2d114a1ef91ab43d00fb76965a75f641642b8a
Colclough7 <ericcolclough@gmail.com>
2023-02-04 21:34:55 -0500

    Update writeNumberInExpandedForm.js

--- writeNumberInExpandedForm.js
+++ writeNumberInExpandedForm.js
@@ -4,3 +4,25 @@ You will be given a number and you will need to return it as a string in Expande
 expandedForm(12); // Should return '10 + 2'
 expandedForm(42); // Should return '40 + 2'
 expandedForm(70304); // Should return '70000 + 300 + 4'*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
