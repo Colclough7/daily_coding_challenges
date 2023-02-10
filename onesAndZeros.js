469abef0834a05db52f79d621eb8054a74e426c6
Colclough7 <ericcolclough@gmail.com>
2023-02-09 22:56:45 -0500

    Update onesAndZeros.js

--- onesAndZeros.js
+++ onesAndZeros.js
@@ -12,3 +12,12 @@ Testing: [0, 0, 1, 0] ==> 2
 Testing: [0, 1, 1, 0] ==> 6
 Testing: [1, 1, 1, 1] ==> 15
 Testing: [1, 0, 1, 1] ==> 11*/
+
+
+
+
+
+const binaryArrayToNumber = arr => {
+  // your code
+  return parseInt(arr.join(""), 2)
+};
