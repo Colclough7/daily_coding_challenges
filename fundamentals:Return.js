249e3eb17f3769a22451ca22cebe4ed38aa74c54
Colclough7 <ericcolclough@gmail.com>
2023-02-15 15:21:53 -0500

    Update fundamentals:Return.js

--- fundamentals:Return.js
+++ fundamentals:Return.js
@@ -1,1 +1,15 @@
+/*Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt*/
