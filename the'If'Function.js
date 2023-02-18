ea7198e166c30298ea0224135d9522b5c926a4b2
Colclough7 <ericcolclough@gmail.com>
2023-02-17 21:25:02 -0500

    Update the'If'Function.js

--- the'If'Function.js
+++ the'If'Function.js
@@ -5,3 +5,12 @@ When bool is truthy, func1 should be called, otherwise call the func2.
 Example:
 _if(true, function(){console.log("True")}, function(){console.log("false")})
 // Logs 'True' to the console.*/
+
+
+
+
+
+function _if(bool, func1, func2) {
+  // ...
+  return bool?func1():func2()
+}
