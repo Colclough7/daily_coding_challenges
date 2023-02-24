8e295e143ac21d82286100106e3fb3ce51e7479e
Colclough7 <ericcolclough@gmail.com>
2023-02-24 03:22:48 -0500

    Update sleighAuthentication.js

--- sleighAuthentication.js
+++ sleighAuthentication.js
@@ -12,4 +12,11 @@ sleigh.authenticate("Santa Claus", "Ho Ho Ho!"); // must return TRUE
 
 sleigh.authenticate("Santa", "Ho Ho Ho!"); // must return FALSE
 sleigh.authenticate("Santa Claus", "Ho Ho!"); // must return FALSE
-sleigh.authenticate("jhoffner", "CodeWars"); // Nope, even Jake is not allowed to use the sleigh ;)*//
+sleigh.authenticate("jhoffner", "CodeWars"); // Nope, even Jake is not allowed to use the sleigh ;)*/
+
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  return name === 'Santa Claus' && password === 'Ho Ho Ho!';
+};
