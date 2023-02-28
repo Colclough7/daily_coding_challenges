b2de9351d170a44930e9ba1445536ff8e9cc2841
Colclough7 <ericcolclough@gmail.com>
2023-02-27 21:31:29 -0500

    Update holidayVI-SharkPontoon.js

--- holidayVI-SharkPontoon.js
+++ holidayVI-SharkPontoon.js
@@ -23,3 +23,26 @@ The pontoon, you, and the shark are all aligned in one dimension.
 If you make it, return "Alive!", if not, return "Shark Bait!".*/
 
 
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+let timeToPontoonPeople = pontoonDistance / youSpeed;
+  let timeToPontoonShark = sharkDistance / sharkSpeed;
+  let timeHalfSharkSpeed = sharkDistance / (sharkSpeed / 2);
+  let timeDifferenceWithDolphin = timeHalfSharkSpeed - timeToPontoonPeople;
+  let timeDifferenceWithoutDolphin = timeToPontoonShark - timeToPontoonPeople;
+
+  if (sharkDistance < sharkSpeed) {
+    return "Shark Bait!";
+  } else if (pontoonDistance < youSpeed) {
+    return "Alive!";
+  }
+
+  if (dolphin === true && timeDifferenceWithDolphin > 0) {
+    return "Alive!";
+  } else if (dolphin === true && timeDifferenceWithDolphin < 0) {
+    return "Shark Bait!";
+  } else if (dolphin === false && timeDifferenceWithoutDolphin > 0) {
+    return "Alive!";
+  } else {
+    return "Shark Bait!";
+  }
+}
