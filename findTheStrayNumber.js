15fd34f55d0a1612ea5ddabea448a14f7281ea3f
Colclough7 <ericcolclough@gmail.com>
2023-03-17 19:18:51 -0400

    Update findTheStrayNumber.js

--- findTheStrayNumber.js
+++ findTheStrayNumber.js
@@ -7,3 +7,13 @@ The input array will always be valid! (odd-length >= 3)
 Examples
 [1, 1, 2] ==> 2
 [17, 17, 3, 17, 17, 17, 17] ==> 3*/
+
+
+
+function stray(numbers) {
+ for (let i = 0; i < numbers.length; i++) {
+    if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+      return numbers[i];
+    }
+  }
+}
