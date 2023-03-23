672b8ac64169e3f7eb3b7dc96c97603fc8c08a4b
Colclough7 <ericcolclough@gmail.com>
2023-03-22 23:54:14 -0400

    Update bestTravel.js

--- bestTravel.js
+++ bestTravel.js
@@ -24,3 +24,9 @@ ts = [50, 55, 56, 57, 58] choose_best_sum(163, 3, ts) -> 163
 xs = [50] choose_best_sum(163, 3, xs) -> nil (or null or ... or -1 (C++, C, D, Rust, Swift, Go, ...)
 
 ys = [91, 74, 73, 85, 73, 81, 87] choose_best_sum(230, 3, ys) -> 228*/
+
+
+
+
+  const chooseBestSum = (t, k, ls) => ls.reduce((r, e) => r.concat(r.filter(c => c.length < k).map(c => c.concat([e]))), [[]])
+  .filter(c => c.length === k).map(c => c.reduce((a, b) => a + b)).filter(s => s <= t).sort((a, b) => b - a)[0] || null;
