3e8ffa2cb9c1102908618efbea0b688375f9d3f8
Colclough7 <ericcolclough@gmail.com>
2023-03-24 14:45:22 -0400

    Update larioAndMuigiPipeProblem.js

--- larioAndMuigiPipeProblem.js
+++ larioAndMuigiPipeProblem.js
@@ -13,3 +13,10 @@ Example
 Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8*/
 
 
+function pipeFix(numbers){
+let arr = [];
+	for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+		arr.push(i);
+	}
+	return arr;
+}
