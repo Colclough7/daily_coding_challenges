d1064d5004f765292a3f6f226efd1549c564fa17
Colclough7 <ericcolclough@gmail.com>
2023-03-25 18:55:24 -0400

    Update removingElements.js

--- removingElements.js
+++ removingElements.js
@@ -4,3 +4,16 @@ Example:
 ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 None of the arrays will be empty, so you don't have to worry about that!*/
+
+
+
+
+
+function removeEveryOther(arr){
+  //your code here
+let result = [];
+  for (let i = 0; i < arr.length; i+=2) {
+    result.push(arr[i]);
+  }
+  return result
+}
