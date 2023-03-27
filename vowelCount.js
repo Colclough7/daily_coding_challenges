b40a9b217280f51588b0f29c26accecde0784f8e
Colclough7 <ericcolclough@gmail.com>
2023-03-26 23:55:44 -0400

    Update vowelCount.js

--- vowelCount.js
+++ vowelCount.js
@@ -3,3 +3,15 @@
 We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.*/
+
+
+
+function getCount(str) {
+var vowelsCounter = 0;
+  str = str.toLowerCase();
+  var vowels = ["a","e","i","o","u"];
+  for (let i of str) {
+    if (vowels.includes(i)) vowelsCounter++;
+  }
+  return vowelsCounter;
+}
