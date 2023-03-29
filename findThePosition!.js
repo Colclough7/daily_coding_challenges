0ace4dbffbc835d1ad866a0c56b31ca2702d3826
Colclough7 <ericcolclough@gmail.com>
2023-03-28 21:19:24 -0400

    Update findThePosition!.js

--- findThePosition!.js
+++ findThePosition!.js
@@ -3,3 +3,17 @@
 Input :: "a"
 
 Ouput :: "Position of alphabet: 1"*/
+
+
+
+
+
+function position(letter){
+//Write your own Code!
+  let abc = 'abcdefghijklmnopqrstuvwxyz'
+  for(let i = 0; i < abc.length; i++){
+    if(letter === abc[i]){
+      return `Position of alphabet: ${i+1}`
+    }
+  }
+}
