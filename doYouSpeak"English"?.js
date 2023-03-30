575a4097d96cfa26070a0c9469dfff197a9c65fb
Colclough7 <ericcolclough@gmail.com>
2023-03-29 23:26:49 -0400

    Update doYouSpeak"English"?.js

--- doYouSpeak"English"?.js
+++ doYouSpeak"English"?.js
@@ -8,3 +8,7 @@ Upper or lower case letter does not matter -- "eNglisH" is also correct.
 Return value as boolean values, true for the string to contains "English", false for it does not.*/
 
 
+function spEng(sentence){
+//write your code here
+  return sentence.toLowerCase().includes('english')
+}
