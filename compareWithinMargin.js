09b2de850d336f69499b69c83b2243201263e6b0
Colclough7 <ericcolclough@gmail.com>
2023-03-31 23:40:46 -0400

    Update compareWithinMargin.js

--- compareWithinMargin.js
+++ compareWithinMargin.js
@@ -27,3 +27,22 @@ Example 2
 If a = 3, b = 5, and margin = 0, then close_compare(a, b, margin) should return -1.
 
 This is because the distance between a and b is greater than 0, and a is less than b.*/
+
+
+
+function closeCompare(a, b, margin){
+  // ...
+   if (!margin) {
+    //When a is lower than b, return -1.
+    if (a < b) return -1;
+    //When a is higher than b, return 1.
+    if (a > b) return 1;
+    // for a === b return should be 0.
+    if (a === b) return 0;
+  } else {
+    //When a is close to b, return 0 | close to ==> margin >= a - b
+    if (margin >= Math.abs(a - b) || a === b) return 0;
+    if (a < b) return -1;
+    if (a > b) return 1;
+  }
+}
