8afedd2ac55a6787d199d8e2415a039722028b42
Colclough7 <ericcolclough@gmail.com>
2023-04-01 23:55:47 -0400

    Update arrayHelpers.js

--- arrayHelpers.js
+++ arrayHelpers.js
@@ -20,3 +20,70 @@ numbers.average(); // must return 3
 numbers.sum();     // must return 15
 numbers.even();    // must return [2, 4]
 numbers.odd();     // must return [1, 3, 5]*/
+
+
+
+
+// TODO
+function checkArr(arr){
+  return Array.isArray(arr) || Object.prototype.toString.call(arr) === '[object Array]';
+}
+
+let arrProto = Array.prototype;
+
+arrProto.square = function(){
+  if(checkArr(this)){
+     return this.map((item) => {
+        return Math.pow(item, 2);
+     });
+  } else {
+    throw 'params must be a array';
+  }
+};
+
+arrProto.cube = function(){
+  if(checkArr(this)){
+     return this.map((item) => {
+        return Math.pow(item, 3);
+     });
+  } else {
+    throw 'params must be a array';
+  }
+};
+
+arrProto.sum = function(){
+  if(checkArr(this)){
+     return this.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue;
+     },0);
+  } else {
+    throw 'params must be a array';
+  }
+};
+
+arrProto.average = function(){
+  if(checkArr(this)){
+     return this.sum() / this.length;
+  } else {
+    throw 'params must be a array';
+  }
+};
+
+arrProto.even = function(){
+   if(checkArr(this)){
+     return this.filter((item) => {
+        return item % 2 === 0;
+     });
+  } else {
+    throw 'params must be a array';
+  }
+};
+
+arrProto.odd = function(){
+   if(checkArr(this)){
+     return this.filter((item) => {
+        return item % 2 !== 0;
+     });
+  } else {
+    throw 'params must be a array';
+  }
