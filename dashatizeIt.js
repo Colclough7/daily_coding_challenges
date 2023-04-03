91d7ee243a3271d9ccecb3203df776d802ee5a8c
Colclough7 <ericcolclough@gmail.com>
2023-04-03 19:39:43 -0400

    Update dashatizeIt.js

--- dashatizeIt.js
+++ dashatizeIt.js
@@ -9,3 +9,21 @@ Ex:
 
 dashatize(274) -> '2-7-4'
 dashatize(6815) -> '68-1-5'*/
+
+
+
+
+function dashatize(num) {
+  //get 'em
+   if (Number.isNaN(num))
+        return 'NaN';
+    return (Math.abs(num).toString().split('').map((n,i,a)=>{
+        let isFirstLetter = i == 0;
+        let isLastLetter = i === a.length - 1;
+        let isOdd = Boolean(+n % 2);
+        let isNextOdd = Boolean(+a[i + 1] % 2);
+        let isPrevOdd = Boolean(+a[i - 1] % 2);
+        return isFirstLetter ? n : (isOdd || isPrevOdd ? `-${n}` : `${n}`);
+    }
+    ).join(''));
+}
