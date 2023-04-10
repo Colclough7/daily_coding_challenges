42c7e9547066d1f97b2d5c1ae5595e89641fb2a1
Colclough7 <ericcolclough@gmail.com>
2023-04-09 23:52:28 -0400

    Update ifYouCanReadThis.js

--- ifYouCanReadThis.js
+++ ifYouCanReadThis.js
@@ -17,3 +17,20 @@ Punctuation should be kept in your return string, but spaces should not.
 Xray should not have a dash within.
 Every word and punctuation mark should be seperated by a space ' '.
 There should be no trailing whitespace*/
+
+
+
+function to_nato(words) {
+	// Go code
+  let arr = words.split('').filter(x=> x!== ' ')
+  let r = ''
+  arr.map((x,i)=>{
+    let letter = x.toLowerCase()
+    let coded = NATO[letter]
+    if(i !== arr.length - 1 && coded) r+=coded + ' '
+    else if(i == arr.length- 1 && coded) r += coded
+    else if(i !== arr.length - 1 && !coded) r += letter + ' '
+    else r+=letter
+  })
+  return r
+}
