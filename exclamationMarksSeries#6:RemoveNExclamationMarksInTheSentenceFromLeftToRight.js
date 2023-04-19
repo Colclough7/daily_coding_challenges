5cf65fff5453654a3a90547631dd361754a6bb68
Colclough7 <ericcolclough@gmail.com>
2023-04-18 20:08:27 -0400

    Update exclamationMarksSeries#6:RemoveNExclamationMarksInTheSentenceFromLeftToRight.js

--- exclamationMarksSeries#6:RemoveNExclamationMarksInTheSentenceFromLeftToRight.js
+++ exclamationMarksSeries#6:RemoveNExclamationMarksInTheSentenceFromLeftToRight.js
@@ -13,3 +13,17 @@ remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
 remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
 remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
 remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"*/
+
+
+
+
+function remove(s,n){
+  //coding and coding....
+   let input = s;
+  while (n) {
+  input = input.replace(/!/, "");
+  n-=1;
+  }
+  return input;
+
+}
