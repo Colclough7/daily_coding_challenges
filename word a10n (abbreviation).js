0aabdb23d4cb74b042c82915858bc2e4079a0ba3
Colclough7 <ericcolclough@gmail.com>
2023-04-23 23:59:41 -0400

    Update word a10n (abbreviation).js

--- word a10n (abbreviation).js
+++ word a10n (abbreviation).js
@@ -4,3 +4,11 @@ Write a function that takes a string and turns any and all "words" (see below) w
 
 A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
 The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+
+
+
+
+function abbreviate(string) {
+  // ...
+  return string.replace(/\w{4,}/g, function(w) { return w[0] + (w.length - 2) + w[w.length - 1] });
+}
