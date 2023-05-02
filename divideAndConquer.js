790114ac622193c41d6da31391f5cc72c50a30b0
Colclough7 <ericcolclough@gmail.com>
2023-05-02 00:18:10 -0400

    Update divideAndConquer.js

--- divideAndConquer.js
+++ divideAndConquer.js
@@ -1,2 +1,15 @@
 /*Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
 Return as a number.*/
+
+
+
+function divCon(x){
+let num = x.filter(item=>typeof item === "number").reduce((acc,i)=>acc+i,0)
+let string = x.filter(item=>typeof item === "string").map(item=>+item).reduce((acc,i)=>acc+i,0)
+  return num - string
+}
+
+
+
+
+
