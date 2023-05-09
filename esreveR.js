3201c7bcd771034e6858a8f56e1f7deefda49370
Colclough7 <ericcolclough@gmail.com>
2023-05-08 22:14:56 -0400

    Update esreveR.js

--- esreveR.js
+++ esreveR.js
@@ -1,3 +1,13 @@
 /*Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
 
 (the dedicated builtin(s) functionalities are deactivated)*/
+
+
+
+
+
+
+let reverse = function(array) {
+  // TODO: program me!
+  return array.reverse()
+}
