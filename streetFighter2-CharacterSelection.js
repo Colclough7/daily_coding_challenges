d4a3e927ae85d0cb8eb1ffefadc6bb5da9d37f98
Colclough7 <ericcolclough@gmail.com>
2023-05-10 19:53:03 -0400

    Update streetFighter2-CharacterSelection.js

--- streetFighter2-CharacterSelection.js
+++ streetFighter2-CharacterSelection.js
@@ -62,3 +62,68 @@ moves = ['right', 'down', 'left', 'left', 'left', 'left', 'right']
 Result:
 
 ['E.Honda', 'Chun Li', 'Ken', 'M.Bison', 'Sagat', 'Dhalsim', 'Sagat']*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function streetFighterSelection(fighters, position, moves){
+  let hoveredCharacters = [];
+  let currentPosition = position;
+  for (let move of moves){
+
+    if (move == 'up'){
+      if(currentPosition[0] == 0){
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      } else{
+        currentPosition[0]--;
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      }
+    }
+    
+    if (move == 'down'){
+      if(currentPosition[0] == 1){
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      } else{
+        currentPosition[0]++;
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      }
+    }
+    
+    if (move == 'left'){
+      if(currentPosition[1] == 0){
+        currentPosition[1] = 5;
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      } else{
+        currentPosition[1]--;
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      }
+    }
+    
+    if (move == 'right'){
+      if(currentPosition[1] == 5){
+        currentPosition[1] = 0;
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      } else{
+        currentPosition[1]++;
+        hoveredCharacters.push(fighters[currentPosition[0]][currentPosition[1]]);
+      }
+    }
+    
+  }
+  
+  return hoveredCharacters;
+}
