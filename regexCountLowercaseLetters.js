21e15cb058ba9dd88e3ead6c3a4f6aa8ef60cd67
Colclough7 <ericcolclough@gmail.com>
2023-06-01 23:24:12 -0400

    Update regexCountLowercaseLetters.js

--- regexCountLowercaseLetters.js
+++ regexCountLowercaseLetters.js
@@ -12,3 +12,14 @@ lowercaseCount(""); ===> 0;
 lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
 
 lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26*/
+
+
+
+
+
+
+
+function lowercaseCount(str){
+    //How many?
+   return (str.match(/[a-z]/g) || []).length
+}
