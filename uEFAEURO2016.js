e5f1b21f2ce97dd6dbd8b7795c290ca561e38cc6
Colclough7 <ericcolclough@gmail.com>
2023-06-03 23:57:02 -0400

    Update uEFAEURO2016.js

--- uEFAEURO2016.js
+++ uEFAEURO2016.js
@@ -3,3 +3,21 @@
 uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
 uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
 uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."*/
+
+
+
+
+
+function uefaEuro2016(teams, scores){
+  // your code...
+  var teamA = teams[0];
+  var teamB = teams[1];
+
+  if (scores[0] > scores[1]) {
+    return 'At match ' + teamA + ' - ' + teamB + ', ' + teamA + ' won!';
+  } else if (scores[0] < scores[1]) {
+    return 'At match ' + teamA + ' - ' + teamB + ', ' + teamB + ' won!';
+  } else if (scores[0] === scores[1]) {
+    return 'At match ' + teamA + ' - ' + teamB + ', teams played draw.';
+  }
+}
